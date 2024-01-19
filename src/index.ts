f4e6265036cca53bb772f6635ac3c96acbe088e9
Tyger375 <edoardo.takanen@gmail.com>
2024-01-19 14:53:08 +0100

    Fixed npm issue

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+export * from "./client";
+export * from "./realtime";
+
+export * from "./types/Badge";
+export * from "./types/Chat";
+export * from "./types/Comment";
+export * from "./types/Message";
+export * from "./types/Post";
+export * from "./types/Reaction";
+export * from "./types/Reply";
+export * from "./types/Report";
+export * from "./types/Tag";
+export * from "./types/User";
+export * from "./types/WsMessage";
+
+export * from "./models/Chat";
+export * from "./models/Comment";
+export * from "./models/Message";
+export * from "./models/Post";
+export * from "./models/Reply";
+export * from "./models/Tag";
+export * from "./models/User";
+
+export * from "./errors/AlreadyReported";
+export * from "./errors/LoginRequired";
+export * from "./errors/Unauthorized";
+export * from "./errors/UnexpectedStatus";
